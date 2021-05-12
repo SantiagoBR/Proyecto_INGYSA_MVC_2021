63714588803020804a352db7db30948f6c87b040
Santiago <santiagobastidasrodriguez@gmail.com>
2021-05-12 16:18:53 -0500

    Migration data

---
+++ Proyecto_INGYSA_MVC_2021/Migrations/202105102359041_ApplicationContextMigrations.Configuration.cs
@@ -0,0 +1,16 @@
+namespace Proyecto_INGYSA_MVC_2021.Migrations
+{
+    using System;
+    using System.Data.Entity.Migrations;
+    
+    public partial class ApplicationContextMigrationsConfiguration : DbMigration
+    {
+        public override void Up()
+        {
+        }
+        
+        public override void Down()
+        {
+        }
+    }
+}
